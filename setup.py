7672ffe4eebc9bdc1ce73d6dd4623484ee8f9815
David Raleigh <davidraleigh@gmail.com>
2019-09-19 18:05:59 -0400

    including stringfield for queries and IN and NOT_IN sets

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ kwargs = {
     'author': 'David Raleigh',
     'author_email': 'david@swiftera.co',
     'license': 'Apache 2.0',
-    'version': '0.2.3',
+    'version': '0.2.5',
     'python_requires': '>3.6.0',
     'packages': ['nsl.stac'],
     'install_requires': [
